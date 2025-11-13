7f5f30feefb8fadaa69f48411c5b8e101ffe3014
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-11-13 13:11:02 -0500

    completed set

---
+++ src/main/java/Chair.java
@@ -0,0 +1,46 @@
+public class Chair {
+    private int legs; 
+    private String material;
+
+    /**
+     * Create a Chair with custom legs and wood material
+     * @param legs legs of the Chair
+     */
+    public Chair(int legs) {
+        this.legs = legs;
+        this.material = "wood";
+    }
+
+    /**
+     * Create a Chair with 4 legs and wood material
+     */
+    public Chair() {
+        this.legs = 4;
+        this.material = "wood";
+    }
+
+    /**
+     * Create a Chair with custom legs and material     * @param legs legs of the Chair
+     * @param material material of the Chair
+     */
+    public Chair(int legs, String material) {
+        this.legs = legs;
+        this.material = material;
+    }
+
+    /**
+     * Get legs of the Chair
+     * @return legs of the Chair
+     */
+    public int getLegs() {
+        return this.legs;
+    }
+
+    /**
+     * Get material of the Chair
+     * @return material of the Chair
+     */
+    public String getMaterial() {
+        return this.material;
+    }
+}
---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,47 @@
+public class Rectangle {
+    private double width; 
+    private double length;
+
+    /**
+     * Create a rectangle with custom width and length 4
+     * @param width Width of the rectangle
+     */
+    public Rectangle(double length) {
+        this.width = 8;
+        this.length = length;
+    }
+
+    /**
+     * Create a rectangle with width 8 and length 4
+     */
+    public Rectangle() {
+        this.width = 8;
+        this.length = 4;
+    }
+
+    /**
+     * Create a rectangle with custom width and length
+     * @param width Width of the rectangle
+     * @param length Length of the rectangle
+     */
+    public Rectangle(double length, double width) {
+        this.width = width;
+        this.length = length;
+    }
+
+    /**
+     * Get width of the rectangle
+     * @return Width of the rectangle
+     */
+    public double getWidth() {
+        return this.width;
+    }
+
+    /**
+     * Get length of the rectangle
+     * @return Length of the rectangle
+     */
+    public double getLength() {
+        return this.length;
+    }
+}
